3c0fdc70ab0105c297f9cf8b0c483275d3e67343
TeoNemsadze <teona.nemsadze.2@iliauni.edu.ge>
2023-10-22 09:02:37 +0400

    Initial Commit

---
+++ Switch/SwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchApp.swift
+//  Switch
+//
+//  Created by teona nemsadze on 22.10.23.
+//
+
+import SwiftUI
+
+@main
+struct SwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
